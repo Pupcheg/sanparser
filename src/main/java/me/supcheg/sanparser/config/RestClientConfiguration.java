06ef66abc3624a5984a98dc2386783a6ba810668
Pupcheg <santos43241@gmail.com>
2025-01-02 18:13:41 +0300

    Added custom cookie setter for RestClient

--- src/main/java/me/supcheg/sanparser/config/RestClientConfiguration.java
+++ src/main/java/me/supcheg/sanparser/config/RestClientConfiguration.java
@@ -1,5 +1,6 @@
 package me.supcheg.sanparser.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.FormHttpMessageConverter;
@@ -8,8 +9,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfiguration {
     @Bean
-    RestClient webClient() {
+    RestClient webClient(
+            @Value("${client.cookie}") String cookie
+    ) {
         return RestClient.builder()
+                .defaultHeaders(httpHeaders -> httpHeaders
+                        .add("Cookie", cookie)
+                )
                 .messageConverters(messageConverters -> messageConverters
                         .add(new FormHttpMessageConverter())
                 )
