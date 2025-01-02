56c8c98c94b63690f2658064556af4b965ddd90e
Pupcheg <santos43241@gmail.com>
2025-01-02 17:55:39 +0300

    Replaced URL#openStream with RestClient in WebUriRecognizer

--- src/main/java/me/supcheg/sanparser/uri/recognizer/web/WebUriRecognizer.java
+++ src/main/java/me/supcheg/sanparser/uri/recognizer/web/WebUriRecognizer.java
@@ -1,16 +1,21 @@
 package me.supcheg.sanparser.uri.recognizer.web;
 
+import lombok.RequiredArgsConstructor;
 import me.supcheg.sanparser.uri.recognizer.UriRecognizer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClient;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Component
 public class WebUriRecognizer implements UriRecognizer {
+    private final RestClient client;
+
     @Override
     public boolean canRecognize(URI uri) {
         return uri.getScheme().equals("http") || uri.getScheme().equals("https");
@@ -19,9 +24,15 @@ public class WebUriRecognizer implements UriRecognizer {
     @Override
     public Optional<InputStream> recognize(URI uri) throws IOException {
         try {
-            return Optional.of(uri.toURL().openStream());
-        } catch (FileNotFoundException e) {
+            return Optional.ofNullable(
+                    client.get()
+                            .uri(uri)
+                            .exchange((request, response) -> response.getBody(), false)
+            );
+        } catch (ResourceAccessException e) {
             return Optional.empty();
+        } catch (Exception e) {
+            throw new IOException(e);
         }
     }
 }
